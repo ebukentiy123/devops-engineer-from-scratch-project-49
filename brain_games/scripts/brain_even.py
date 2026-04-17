631c0f684f600a39b38100fadb342f196ce5eb1a
Mikhail Fedorov <mf1511@yandex.ru>
2026-04-17 19:13:45 +0300

    update brain_games.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -17,24 +17,29 @@ def main_logic():
     print(f"Hello, {name}!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
-    counter = 1
+    counter = 0
 
-    while counter <= 3:
+    while counter <= 2:
         random_number = randint(0, 100)
         print(f"Question: {random_number}")
         answer = prompt.string("Your answer: ")
         result = is_even(random_number)
+        correct_answer = "yes" if result else "no"
+        
         if answer == "no" and not result:
             print("Correct!")
             counter = counter + 1
         elif answer == "yes" and result:
             print("Correct!")
             counter = counter + 1
-        else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+            
+        if answer != correct_answer:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
             break
-        
-    print(f"Congratulations, {name}!")
+
+    if counter == 3:
+        print(f"Congratulations, {name}!")
 
 
 def main():
