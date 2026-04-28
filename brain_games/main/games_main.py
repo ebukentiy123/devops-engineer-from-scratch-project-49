f4d5650ce97e45cb6411bf9818b3ae2f3fe17a5c
Mikhail Fedorov <mf1511@yandex.ru>
2026-04-28 14:08:37 +0300

    change line 20. Print split into two lines

--- brain_games/main/games_main.py
+++ brain_games/main/games_main.py
@@ -17,7 +17,8 @@ def run_game(game):
         if answer == correct_answer:
             print("Correct!")
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer", end=" ")
+            print(f";(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
